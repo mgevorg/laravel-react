c8ba21044477701eba35b43657bd023bd7dcb395
Gevorg Manukyan <gevorg.manukyan@yahoo.com>
2025-02-15 20:40:36 +0400

    .

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Services\AuthService\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', [AuthController::class, 'user']);
